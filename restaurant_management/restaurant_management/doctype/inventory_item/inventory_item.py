32cf86aad2223156cdca960101e9aa3837d10042
R-Jayaraman <jayaramanoffl@gmail.com>
2026-08-14 19:21:08 +0530

    feat(inventory): improve inventory item processing

--- restaurant_management/restaurant_management/doctype/inventory_item/inventory_item.py
+++ restaurant_management/restaurant_management/doctype/inventory_item/inventory_item.py
@@ -1,9 +1,15 @@
 # Copyright (c) 2026, Ram and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.utils import flt
 
 
 class InventoryItem(Document):
-	pass
+	def validate(self):
+		if flt(self.standard_rate) < 0:
+			frappe.throw("Standard Rate cannot be negative.")
+
+		if flt(self.reorder_level) < 0:
+			frappe.throw("Reorder Level cannot be negative.")
