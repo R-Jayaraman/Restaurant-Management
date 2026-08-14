bf969b23fe0062f8084ec7d96b5b4cb90c60935f
R-Jayaraman <jayaramanoffl@gmail.com>
2026-08-14 19:20:01 +0530

    feat(staff): update staff accounting integration

--- restaurant_management/restaurant_management/doctype/staff/staff.py
+++ restaurant_management/restaurant_management/doctype/staff/staff.py
@@ -1,9 +1,12 @@
 # Copyright (c) 2026, Ram and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.utils import flt
 
 
 class Staff(Document):
-	pass
+	def validate(self):
+		if flt(self.salary) < 0:
+			frappe.throw("Salary cannot be negative.")
