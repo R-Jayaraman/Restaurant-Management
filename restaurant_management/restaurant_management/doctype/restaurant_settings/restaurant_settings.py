b40f74be93b62945df9a56f9309d1ae8c884b720
R-Jayaraman <jayaramanoffl@gmail.com>
2026-08-14 19:21:40 +0530

    feat(stock): improve stock transaction and restaurant settings

--- restaurant_management/restaurant_management/doctype/restaurant_settings/restaurant_settings.py
+++ restaurant_management/restaurant_management/doctype/restaurant_settings/restaurant_settings.py
@@ -1,9 +1,11 @@
 # Copyright (c) 2026, Ram and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class RestaurantSettings(Document):
-	pass
+	def validate(self):
+		if self.opening_time and self.closing_time and self.opening_time >= self.closing_time:
+			frappe.throw("Opening Time must be before Closing Time.")
--- restaurant_management/restaurant_management/doctype/stock_transaction/stock_transaction.py
+++ restaurant_management/restaurant_management/doctype/stock_transaction/stock_transaction.py
@@ -1,9 +1,20 @@
 # Copyright (c) 2026, Ram and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.utils import flt
 
 
 class StockTransaction(Document):
-	pass
+	def validate(self):
+		qty = flt(self.qty)
+
+		if qty == 0:
+			frappe.throw("Qty cannot be zero.")
+
+		if self.transaction_type == "Purchase" and qty < 0:
+			frappe.throw("Qty must be positive for a Purchase transaction.")
+
+		if self.transaction_type == "Waste" and qty > 0:
+			frappe.throw("Qty must be negative for a Waste transaction.")
