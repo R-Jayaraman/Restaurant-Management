6c55d27e778676fbb47c61d37e837c7d6403c163
R-Jayaraman <jayaramanoffl@gmail.com>
2026-08-14 19:19:34 +0530

    feat(menu): enhance menu item accounting and client logic

--- restaurant_management/restaurant_management/doctype/menu_item/menu_item.py
+++ restaurant_management/restaurant_management/doctype/menu_item/menu_item.py
@@ -1,9 +1,15 @@
 # Copyright (c) 2026, Ram and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.utils import flt
 
 
 class MenuItem(Document):
-	pass
+	def validate(self):
+		if flt(self.price) <= 0:
+			frappe.throw("Price must be greater than zero.")
+
+		if flt(self.tax_rate) < 0 or flt(self.tax_rate) > 100:
+			frappe.throw("Tax Rate must be between 0 and 100.")
