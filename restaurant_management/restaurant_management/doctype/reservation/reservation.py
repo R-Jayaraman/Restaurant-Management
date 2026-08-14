aa602ef04f9e07b1c62378d5270e8086a89797e7
R-Jayaraman <jayaramanoffl@gmail.com>
2026-08-14 19:19:51 +0530

    feat(reservation): improve reservation workflow and validation

--- restaurant_management/restaurant_management/doctype/reservation/reservation.py
+++ restaurant_management/restaurant_management/doctype/reservation/reservation.py
@@ -1,9 +1,11 @@
 # Copyright (c) 2026, Ram and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class Reservation(Document):
-	pass
+	def validate(self):
+		if self.party_size is not None and self.party_size <= 0:
+			frappe.throw("Party Size must be greater than zero.")
